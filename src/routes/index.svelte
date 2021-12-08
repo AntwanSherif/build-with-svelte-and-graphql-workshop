5c9eb9ed4078b99f0646e8cca5ffa18ad2d43bd1
AntwanSherif <antwansherif@gmail.com>
2021-12-08 16:49:28 +0200

    :sparkles: Get Get products from GraphQL endpoint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    import { gql, GraphQLClient } from "graphql-request";
+
+    export const load = async () => {
+     const client = new GraphQLClient("https://api-eu-central-1.graphcms.com/v2/ckwxmf4xi02w301z03ze7hn4w/master");
+
+     const query = gql`
+      {
+        posts {
+          title
+          slug
+          date
+          excerpt
+          tags
+          coverImage {
+            url
+          }
+        }
+      }
+    `
+
+  const {posts} = await client.request(query);
+
+  return {
+      props: {
+          posts
+      }
+  }
+}
+
+</script>
+
+<script>
+    export let posts
+</script>
+
+<pre>{JSON.stringify(posts, null, 2)}</pre>
