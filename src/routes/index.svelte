920aa389a0ffb43be2273aa65df0718b60f47035
AntwanSherif <antwansherif@gmail.com>
2021-12-08 17:02:10 +0200

    :sparkles: Add Tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,37 +1,45 @@
 <script context="module">
-    import { gql, GraphQLClient } from "graphql-request";
+	import { gql, GraphQLClient } from 'graphql-request';
 
-    export const load = async () => {
-     const client = new GraphQLClient("https://api-eu-central-1.graphcms.com/v2/ckwxmf4xi02w301z03ze7hn4w/master");
+	export const load = async () => {
+		const client = new GraphQLClient(
+			'https://api-eu-central-1.graphcms.com/v2/ckwxmf4xi02w301z03ze7hn4w/master'
+		);
 
-     const query = gql`
-      {
-        posts {
-          title
-          slug
-          date
-          excerpt
-          tags
-          coverImage {
-            url
-          }
-        }
-      }
-    `
+		const query = gql`
+			{
+				posts {
+					title
+					slug
+					date
+					excerpt
+					tags
+					coverImage {
+						url
+					}
+				}
+			}
+		`;
 
-  const {posts} = await client.request(query);
+		const { posts } = await client.request(query);
 
-  return {
-      props: {
-          posts
-      }
-  }
-}
-
+		return {
+			props: {
+				posts
+			}
+		};
+	};
 </script>
 
 <script>
-    export let posts
+	export let posts;
 </script>
 
-<pre>{JSON.stringify(posts, null, 2)}</pre>
+{#each posts as { title, slug, date, excerpt, tags, coverImage }}
+	<li>
+		<p>{title}</p>
+		<p>{excerpt}</p>
+		<img src={coverImage.url} alt={coverImage.alt} />
+	</li>
+	>
+{/each}
